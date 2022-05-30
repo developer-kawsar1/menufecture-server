9cbd2637bbe39aa02db0408342306e94bd8334fe
kawsar ahmed <webdeveloper.kawsar99@gmail.com>
2022-05-30 15:54:08 +0600

    added some route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const cors = require('cors'); 
+const cors = require('cors');  
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
 const app = express()
@@ -20,6 +21,7 @@ async function run(){
       const serviceCollection = client.db('menufactureBD').collection('product')
       const reviewCollection = client.db('reviewDb').collection('review')
       const orderCollection = client.db('orderDb').collection('order')
+      const usersCollection = client.db('usersDb').collection('user')
       app.get('/product', async (req, res) => {
         const query = {}
         const cursor = serviceCollection.find(query);
@@ -38,7 +40,41 @@ async function run(){
         const orders = await cursor.toArray()
         res.send(orders)
       }) 
+      app.get('/user',  async (req, res) => {
+        const users = await usersCollection.find().toArray();
+        res.send(users);
+      }); 
+      app.get('/user/:id', async(req,res)=>{
+        const id=req.params.id
+        const query={_id:ObjectId(id)} 
+        const user= await usersCollection.findOne(query)
+        res.send(user) 
+        // console.log(user);
+      }) 
       // single item api 
+      app.get('/user/:email', async(req,res)=>{
+        const id=req.params.id
+        const query={_id:ObjectId(id)} 
+        const product= await serviceCollection.findOne(query)
+        res.send(product)
+    }) 
+// put api
+
+app.put('/user/:email',async (req,res)=>{
+  const email = req.params.email; 
+  const filter = { email: email };  
+  const user = req.body;
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: user,
+  }; 
+  const result = await usersCollection.updateOne(filter, updateDoc, options); 
+  const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+  res.send( {result,token});
+}) 
+
+
+
 
          // post  api
     app.post('/reviews',async(req,res)=>{
@@ -50,8 +86,20 @@ async function run(){
       
 
   }) 
+  app.get('/api/users', function(req, res) {
+    const user_id = req.query.id;
+    const token = req.query.token;
+    const geo = req.query.geo;
+  
+    res.send({
+      'user_id': user_id,
+      'token': token,
+      'geo': geo
+    });
+  });
     app.post('/orders',async(req,res)=>{
       const newOrder=req.body 
+      console.log(req.query);
       console.log(newOrder);
       const result=await orderCollection .insertOne(newOrder) 
       res.send(result) 
